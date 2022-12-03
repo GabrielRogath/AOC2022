553c736998a7d871c61fb4585117f186c92c2de0
GabrielRogath <gassenga080@gmail.com>
2022-12-03 14:42:31 +0300

    updateQn2

---
+++ DayTwo/soln.js
@@ -0,0 +1,10 @@
+
+const fs = require('fs');
+const input = fs.readFileSync("D:\\Advent-Of-Code2022\\DayTwo\\sample.txt", { encoding: "utf-8" }).split("\r\n");
+
+function partOne(){
+
+    console.log(input);
+}
+
+partOne();
